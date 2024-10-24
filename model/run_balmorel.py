6c4cb67bb6661cfa5795cd7174e45f0b7b326036
thbal <thbal@dtu.dk>
2024-10-24 14:23:01 +0200

    EU update

--- model/run_balmorel.py
+++ model/run_balmorel.py
@@ -20,30 +20,31 @@ def run_scenario(index, sample):
     base_data = Container(load_from="../scenario_data/input_data/input_data_baseline.gdx")
     scenario_data = copy(base_data)
     
-    North_l_PV = ["DK","NO","SE","NL", "DE4-E","DE4-N","DE4-W","FIN", "UK","EE","LV","LT","PL","BE"]
-    North_PV = '|'.join(North_l_PV)
-    South_l_PV = ["DE4-S","FR","IT","CH","AT","CZ","ES","PT","SK","HU","SI","HR","RO","BG","GR","IE","LU","AL","ME","MK","BA","RS","TR","MT","CY"]
-    South_PV = '|'.join(South_l_PV)
-    SUBTECHGROUPKPOT = scenario_data["SUBTECHGROUPKPOT"].records
-    SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="SOLARPV") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(North_PV)), "value"]*= sample["PV_LIMIT_NORTH"]
-    SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="SOLARPV") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(South_PV)), "value"]*= sample["PV_LIMIT_SOUTH"]
-   
- 
-    North_l = ["NO","SE","NL","FIN", "UK","EE","LV","LT","PL","BE"]
+    North_l = ["DK","NO","SE", "FIN", "EE","LV","LT"]
     North = '|'.join(North_l)
-    South_l = ["FR","IT","CH","AT","CZ","ES","PT","SK","HU","SI","HR","RO","BG","GR","IE","LU","AL","ME","MK","BA","RS","TR","MT","CY"]
+    South_l = ["IT", "ES","PT","SI","HR","AL","MT","CY", "BA","ME","MK","RS"]
     South = '|'.join(South_l)
-    SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains("DK")), "value"]*= sample["ONS_LIMIT_DK"]
-    SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains("DE")), "value"]*= sample["ONS_LIMIT_DE"]
+    East_l = ["PL","CZ","SK","RO","BG","HU",]
+    East = '|'.join(East_l)
+    West_l = ["FR", "DE","NL","UK", "BE", "LU", "AT","CH", "IE" ]
+    West = '|'.join(West_l)
+    
+    SUBTECHGROUPKPOT = scenario_data["SUBTECHGROUPKPOT"].records
+    SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="SOLARPV") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(North)), "value"]*= sample["PV_LIMIT_NORTH"]
+    SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="SOLARPV") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(South)), "value"]*= sample["PV_LIMIT_SOUTH"]
+    SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="SOLARPV") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(East)), "value"]*= sample["PV_LIMIT_EAST"]
+    SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="SOLARPV") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(West)), "value"]*= sample["PV_LIMIT_WEST"]
+
+    SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(East)), "value"]*= sample["ONS_LIMIT_EAST"]
+    SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(West)), "value"]*= sample["ONS_LIMIT_WEST"]
     SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(North)), "value"]*= sample["ONS_LIMIT_NORTH"]
     SUBTECHGROUPKPOT.loc[(SUBTECHGROUPKPOT["TECH_GROUP"]=="WINDTURBINE_ONSHORE") & (SUBTECHGROUPKPOT["CCCRRRAAA"].str.contains(South)), "value"]*= sample["ONS_LIMIT_SOUTH"]
    
     HYDROGEN_DH2 = scenario_data["HYDROGEN_DH2"].records
-    HYDROGEN_DH2.loc[HYDROGEN_DH2["CCCRRRAAA"].str.contains("DK"), "value"] *= sample["DH2_DEMAND_DK"]
-    North_and_South_l = ["NO","SE","NL","FIN", "UK","EE","LV","LT","PL","BE","FR","IT","CH","AT","CZ","ES","PT","SK","HU","SI","HR","RO","BG","GR","IE","LU","AL","ME","MK","BA","RS","TR","MT","CY"]
-    North_and_South = '|'.join(North_and_South_l)
-    HYDROGEN_DH2.loc[HYDROGEN_DH2["CCCRRRAAA"].str.contains("DE"), "value"] *= sample["DH2_DEMAND_DE"]
-    HYDROGEN_DH2.loc[HYDROGEN_DH2["CCCRRRAAA"].str.startswith((North_and_South)), "value"] *= sample["DH2_DEMAND_Rest"]
+    HYDROGEN_DH2.loc[HYDROGEN_DH2["CCCRRRAAA"].str.contains(North), "value"] *= sample["DH2_DEMAND_NORTH"]
+    HYDROGEN_DH2.loc[HYDROGEN_DH2["CCCRRRAAA"].str.contains(South), "value"] *= sample["DH2_DEMAND_SOUTH"]
+    HYDROGEN_DH2.loc[HYDROGEN_DH2["CCCRRRAAA"].str.contains(East), "value"] *= sample["DH2_DEMAND_EAST"]
+    HYDROGEN_DH2.loc[HYDROGEN_DH2["CCCRRRAAA"].str.contains(West), "value"] *= sample["DH2_DEMAND_WEST"]
 
     EMI_POL = scenario_data["EMI_POL"].records
     EMI_POL.loc[(EMI_POL["CCCRRRAAA"]=="DENMARK") & (EMI_POL["GROUP"]=="ALL_SECTORS") & (EMI_POL["EMIPOLSET"]=="TAX_CO2"), "value"] *= sample["CO2_TAX"]
@@ -71,9 +72,11 @@ def run_scenario(index, sample):
     FUELPRICE.loc[FUELPRICE["FFF"]=="IMPORT_H2","value"] *= sample["IMPORT_H2_P"]
     
     DE = scenario_data["DE"].records
-    DE.loc[DE["RRR"].str.contains("DK"), "value"] *= sample["DE_DEMAND_DK"]
-    DE.loc[DE["RRR"].str.contains("DE"), "value"] *= sample["DE_DEMAND_DE"]
-    DE.loc[~DE["RRR"].str.contains("DK|DE"), "value"] *= sample["DE_DEMAND_Rest"]
+    DE.loc[DE["RRR"].str.contains(North), "value"] *= sample["DE_DEMAND_NORTH"]
+    DE.loc[DE["RRR"].str.contains(South), "value"] *= sample["DE_DEMAND_SOUTH"]
+    DE.loc[DE["RRR"].str.contains(East), "value"] *= sample["DE_DEMAND_EAST"]
+    DE.loc[DE["RRR"].str.contains(West), "value"] *= sample["DE_DEMAND_WEST"]
+    #DE.loc[~DE["RRR"].str.contains("DK|DE"), "value"] *= sample["DE_DEMAND_Rest"]
 
     scenario_data.write("../scenario_data/input_data/input_data_scenario_{}.gdx".format(index+1))
     os.system("gams ./Balmorel_finish.gms --id=scenario_{0} r=s1 > ../scenario_data/log_files/output_file_scenario_{0}.txt".format(index+1))
