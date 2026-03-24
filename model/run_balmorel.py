e5cdf33f12445a463ba9c3406bbc0cb70c371790
thbal <thbal@dtu.dk>
2026-03-24 12:11:24 +0100

    your message

--- model/run_balmorel.py
+++ model/run_balmorel.py
@@ -16,51 +16,54 @@ def get_arg():
     parser.add_argument('--nb_scen', default=1, type=int, help='Number of scenarios (integer)')
     parser.add_argument('--input_sample', default="input_params.csv", type=str, help='Name of the input sampling csv file (str)')
     parser.add_argument('--nb_cores', default=2, type=int, help='Number of cores (integer)')
+    parser.add_argument('--threads', '-t', default=1, type=int, help='Number of threads for each scenario (integer)')
+    parser.add_argument('--path', default='scenario_data', type=str, help='Path to save scenario data')
+    parser.add_argument('--sampler', default='LHC', type=str, help='Sampling strategy used for sampling (Morris, Sobol, LHC, FAST)')
     args = parser.parse_args()
-    return args.nb_scen, args.input_sample, args.nb_cores
+    return args.nb_scen, args.input_sample, args.nb_cores, args.threads, args.path, args.sampler
 
-def run_scenario(index, sample, parameters):
+def run_scenario(index, sample, parameters, rpath, threads):
     print("Running scenario {}".format(index+1))
-    os.system("gams ./Balmorel_finish.gms --id=scenario_{0} r=s1 > ../scenario_data/log_files/output_file_scenario_{0}.txt".format(index+1))
+    os.system("gams ./Balmorel_finish.gms --id=scenario_{0} --rpath={1} r=s1 threads={2} > ../{1}/log_files/output_file_scenario_{0}.txt".format(index+1, rpath, threads))
 
 if __name__ == '__main__': 
-    if not os.path.isdir("../scenario_data"):
-        os.makedirs("../scenario_data")
-        os.makedirs("../scenario_data/log_files")
-        os.makedirs("../scenario_data/input_data")
-        os.makedirs("../scenario_data/output_data")
-        
-    # Arguments
-    num_scen, input_file, nb_cores = get_arg()
+    num_scen, input_file, nb_cores, threads, rpath, sampling_strategy = get_arg()
+    if not os.path.isdir("../{}".format(rpath)):
+        os.makedirs("../{}".format(rpath))
+        os.makedirs("../{}/log_files".format(rpath))
+        os.makedirs("../{}/input_data".format(rpath))
+        os.makedirs("../{}/output_data".format(rpath))
     
     # Copy input csv file to scenario data input data folder (more easy for gams part)
-    os.system("cp ../GSA_parameters/{} ../scenario_data/input_data/input.csv".format(input_file))
+    os.system("cp ../GSA_parameters/{} ../{}/input_data/input.csv".format(input_file, rpath))
     
     # Sampling
-    sampler = sampler("LHC", input="../scenario_data/input_data/input.csv", N=num_scen, rng=42)
+    sampler = sampler(sampling_strategy, input="../{}/input_data/input.csv".format(rpath), N=num_scen, rng=42)
     sampler.sample()
-    sampler.save_samples("../scenario_data/input_data/samples.txt")
+    sampler.save_samples("../{}/input_data/samples.txt".format(rpath))
     samples = pd.DataFrame(sampler.samples, columns = sampler.problem["names"])
     
     # Get the base data of the sets we are going to change and launch the baseline
-    parameters = GSA_parameters(input_file = "../scenario_data/input_data/input.csv")
+    parameters = GSA_parameters(input_file = "../{}/input_data/input.csv".format(rpath))
     sets = parameters.load_sets()
-    os.system('gams ./Balmorel_ReadData.gms --params="{}" s=s1 > ../scenario_data/log_files/output_file_baseline.txt'.format(sets))
-    os.system('gams ./Balmorel_finish.gms --id=baseline r=s1 > ../scenario_data/log_files/output_file_baseline2.txt')
+    os.system('gams ./Balmorel_ReadData.gms --params="{}" s=s1 > ../{}/log_files/output_file_baseline.txt'.format(sets, rpath))
+    os.system('gams ./Balmorel_finish.gms --id=baseline --rpath={1} r=s1 threads={0}> ../{1}/log_files/output_file_baseline2.txt'.format(nb_cores-1, rpath))
     
     # Loop for multi-core launch
     tic = time.time()
-    pool = mp.Pool(processes = nb_cores-2)
-    results = pool.starmap_async(run_scenario, [(index, sample, parameters) for index, sample in samples.iterrows()])
+    # Calculate number of scenarios to run in parallel based on the number of cores and threads
+    num_processes = nb_cores // threads
+    pool = mp.Pool(processes =num_processes)
+    results = pool.starmap(run_scenario, [(index, sample, parameters, rpath, threads) for index, sample in samples.iterrows()])
     pool.close()
     pool.join()
 
-    # Merge the results files
-    merge_cmd = "gdxmerge"
-    for id in range(len(samples)):
-        merge_cmd += " ../scenario_data/output_data/Results_scenario_{}.gdx".format(id+1)
-    merge_cmd += " output=../scenario_data/output_data/Results_merged.gdx"
-    os.system(merge_cmd)
+    # # Merge the results files
+    # merge_cmd = "gdxmerge"
+    # for id in range(len(samples)):
+    #     merge_cmd += " ../{}/output_data/Results_scenario_{}.gdx".format(rpath, id+1)
+    # merge_cmd += " output=../{}/output_data/Results_merged.gdx".format(rpath)
+    # os.system(merge_cmd)
     
     # # Merge the input data files -> Not working for now
     # merge_cmd = "gdxmerge"
